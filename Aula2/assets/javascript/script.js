747afaae13da076961523cac35d1d79a93aab629
Helder Meyer <helder.meyer3010@gmail.com>
2023-01-28 11:06:22 -0300

    Exercícios Javascript Aula 1 e 2

---
+++ Aula2/assets/javascript/script.js
@@ -0,0 +1,26 @@
+//Para mostrar mensagens no Console
+//console.log('Minha primeira mensagem!')
+//Para mostrar mensagens na tela 
+//window.alert('Minha primeira mensagem!') 
+//Para mostrar mensagem na tela, pedindo confirmação
+//window.confirm('Está gostando de JavaScript?')
+//Para mostrar mensagem na tela, pedindo uma informação
+//const teste = window.prompt('Qual é o seu nome?')
+
+//window.alert('Seu nome é ' + teste)
+
+/*
+const botao = document.getElementById('jeremias')
+botao.addEventListener('click', mudarCor)
+let apertar = 0
+function mudarCor(){
+    if(apertar == 0){
+        document.getElementById('principal').style.color = 'red'
+        apertar = 1
+    }else{
+        document.getElementById('principal').style.color = 'white'
+        apertar = 0
+    }
+}
+*/
+
