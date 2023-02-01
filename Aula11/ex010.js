c334e18a58a6f7914708503ab16299c819a9521a
Helder Meyer <helder.meyer3010@gmail.com>
2023-02-01 17:31:48 -0300

    Exercícios Javascript Aula11

---
+++ Aula11/ex010.js
@@ -0,0 +1,7 @@
+var velocidade = 60
+console.log(`A velocidade do seu carro é ${velocidade} km/h!`)
+if(velocidade > 60){ //Condição simples
+    console.log('Você ultrapassou a velocidade permitida! Multado!')
+}else{ //Condição composta
+    console.log('Dirija sempre usando cinto de segurança!')
+}
