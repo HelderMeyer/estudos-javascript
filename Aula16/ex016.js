cf41cbaf9de4eef38d48e9343812fa73b909b305
Helder Meyer <helder.meyer3010@gmail.com>
2023-02-02 16:34:30 -0300

    Exercícios Javascript Aula16

---
+++ Aula16/ex016.js
@@ -0,0 +1,14 @@
+var agora = new Date() //Acessar o date
+var hora = agora.getHours() //Pegar as horas
+
+console.log(`Agora são exatamente ${hora} horas!`)
+
+if(hora < 5){
+    console.log('Boa madrugada!')
+}else if (hora < 12){
+    console.log('Bom dia!')
+}else if (hora <=18){
+    console.log('Boa tarde!')
+}else {
+    console.log('Boa noite!')
+}
