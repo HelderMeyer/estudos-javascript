5dde2dd62f3e755bb381451ed3bfc765dd683d4f
Helder Meyer <helder.meyer3010@gmail.com>
2023-02-02 16:17:04 -0300

    Exercícios Javascript Aula15

---
+++ Aula15/ex015.js
@@ -0,0 +1,24 @@
+var idade = 64
+
+console.log(`Você tem ${idade} anos!`)
+
+idade < 16 ? //Se a idade for menor do que 16
+
+console.log('Não vota!') : //Não vota
+
+idade < 18 || idade >= 65 ? //Caso contrário, se a idade for maior ou igual a 16 E menor do que 18 ou maior ou igual a 65 anos
+
+console.log('Voto opcional!') : //Voto opcional
+
+console.log('Voto obrigatório!') //Se for maior do que 18 e menor do que 65, voto obrigatório
+
+/*
+ternário
+
+condição ? se for true : se for false
+
+ternário dentro de ternário
+
+condição ? se for true : se for false, condição2 ? se for true : se for false
+*/
+    
