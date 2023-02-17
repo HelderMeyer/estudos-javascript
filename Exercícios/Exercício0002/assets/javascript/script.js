5a473db5a33ed1d320b0566ac96312bdc468c129
Helder Meyer <helder.meyer3010@gmail.com>
2023-02-17 16:04:27 -0300

    Exercício de Condicionais

---
+++ Exercícios/Exercício0002/assets/javascript/script.js
@@ -0,0 +1,21 @@
+let x = 10
+let y = 5
+
+console.log(x > y) //True
+
+let w = "10"
+let z = 10
+
+console.log(w === z) //False
+
+let a = 10
+let b = 5
+
+console.log(x != y)
+
+let preco = 40.3
+if(preco < 45){
+    console.log('Está barato!')
+}else{
+    console.log('Está caro!')
+}
