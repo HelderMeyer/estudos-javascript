818db7ecadf43d69f7e1603767d3b0b6c22e25d0
Helder Meyer <helder.meyer3010@gmail.com>
2023-02-20 15:37:34 -0300

    Exercício de Arrays

---
+++ Exercícios/Exercício0006/assets/javascript/script.js
@@ -0,0 +1,11 @@
+let carros = ['BMW', 'Ferrari', 'Mercedes']
+let x = 1
+console.log(`${carros[x]}`)
+
+carros[1] = 'Audi'
+console.log(carros)
+
+carros.push('Volvo')
+console.log(carros)
+
+console.log(carros.length)
