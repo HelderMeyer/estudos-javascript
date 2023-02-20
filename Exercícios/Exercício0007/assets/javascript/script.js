dc778e212c27dc7eec5502083eeda01d993f6a07
Helder Meyer <helder.meyer3010@gmail.com>
2023-02-20 16:54:32 -0300

    Exercício de Loops

---
+++ Exercícios/Exercício0007/assets/javascript/script.js
@@ -0,0 +1,11 @@
+let frutas = ['Maça', 'Uva', 'Banana']
+for(let i = 0; i < frutas.length; i++){
+    console.log(frutas[i])
+}
+
+let contador = 0
+
+while(contador <= 10){
+    console.log(contador)
+    contador += 1
+}
