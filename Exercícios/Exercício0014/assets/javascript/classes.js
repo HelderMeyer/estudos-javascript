0c3c9c611783e5b2fe6bcd13100d2ed8cf2776cc
Helder Meyer <helder.meyer3010@gmail.com>
2023-02-25 13:42:53 -0300

    Projeto Luta Concluído

--- Exercícios/Exercício0014/assets/javascript/classes.js
+++ Exercícios/Exercício0014/assets/javascript/classes.js
@@ -58,20 +58,71 @@ class BigMonster extends Character {
 }
 
 class Stage {
-    constructor(fighter1, fighter2, fighter1Element, fighter2Element) {
+    constructor(fighter1, fighter2, fighter1Element, fighter2Element, logObject) {
         this.fighter1 = fighter1
         this.fighter2 = fighter2
         this.fighter1Element = fighter1Element
         this.fighter2Element = fighter2Element
+        this.log = logObject
     }
     start() {
         // TODO: Evento do botão de atacar
         this.update()
+        this.fighter1Element = document.querySelector('#attackButtonFighter1').addEventListener('click', () => this.doAttack(this.fighter1, this.fighter2))
+        this.fighter2Element = document.querySelector('#attackButtonFighter2').addEventListener('click', () => this.doAttack(this.fighter2, this.fighter1))
     }
     update() {
         // Fighter1
-        this.fighter1Element = document.querySelector('#nameChar').innerHTML = this.fighter1.name
+        this.fighter1Element = document.querySelector('#nameChar').innerHTML = `${this.fighter1.name} - ${this.fighter1.life.toFixed(2)} HP`
+        let f1Pct = (this.fighter1.life / this.fighter1.maxLife) * 100
+        this.fighter1Element = document.querySelector('#barChar').style.width = `${f1Pct}%`
+        this.fighter1Element = document.querySelector('#barChar').style.transition = `0.5s`
         // Fighter2
-        this.fighter2Element = document.querySelector('#nameMonster').innerHTML = this.fighter2.name
+        this.fighter2Element = document.querySelector('#nameMonster').innerHTML = `${this.fighter2.name} - ${this.fighter2.life.toFixed(2)} HP`
+        let f2Pct = (this.fighter2.life / this.fighter2.maxLife) * 100
+        this.fighter2Element = document.querySelector('#barMonster').style.width = `${f2Pct}%`
+        this.fighter2Element = document.querySelector('#barMonster').style.transition = `0.5s`
+    }
+    doAttack(attacking, attacked) {
+        let attackFactor = (Math.random() * 2).toFixed(2)
+        let defenseFactor = (Math.random() * 2).toFixed(2)
+        let actualAttack = attacking.attack * attackFactor
+        let actualDefense = attacked.defense * defenseFactor
+
+        if (actualAttack > actualDefense) {
+            attacked.life -= actualAttack
+            this.log.addMessage(`${attacking.name} causou ${actualAttack.toFixed(2)} de dano em ${attacked.name}`)
+        } else {
+            this.log.addMessage(`${attacked.name} conseguiu defender...`)
+        }
+        if (attacked.life <= 0) {
+            this.log.addMessage(`${attacking.name} derrotou ${attacked.name}`)
+            document.querySelector('#attackButtonFighter1').style.display = 'none'
+            document.querySelector('#attackButtonFighter2').style.display = 'none'
+            const buttonReset = document.querySelector('button#buttonResetGame')
+            buttonReset.style.display = 'block'
+            buttonReset.addEventListener('click', function () { location.reload() })
+            attacked.life -= actualAttack
+        }
+        this.update()
+    }
+}
+
+class Log {
+    list = []
+    constructor(listElement) {
+        this.listElement = listElement
+    }
+    addMessage(msg) {
+        this.list.push(msg)
+        this.render()
+    }
+    render() {
+        this.listElement.innerHTML = ''
+        for (let i in this.list) {
+            const myLi = document.createElement('li')
+            myLi.innerHTML = this.list[i]
+            this.listElement.appendChild(myLi)
+        }
     }
 }
--- Exercícios/Exercício0014/assets/javascript/script.js
+++ Exercícios/Exercício0014/assets/javascript/script.js
@@ -1,11 +1,13 @@
-let char = new Knight('Helder')
-let monster = new LittleMonster()
+let log = new Log(document.querySelector('ul#log'))
+let char = new Sorcerer('Helder')
+let monster = new BigMonster()
 
 const stage = new Stage(
     char,
     monster,
     document.querySelector('#char'),
-    document.querySelector('#monster')
+    document.querySelector('#monster'),
+    log
 )
 
 stage.start()
