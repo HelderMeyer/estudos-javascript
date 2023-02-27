e0686dba4a3ed5f85814fb829411a8637a63725d
Helder Meyer <helder.meyer3010@gmail.com>
2023-02-27 17:23:13 -0300

    Projeto Luta com Programação Funcional

---
+++ Exercícios/Exercício0015/assets/javascript/functions.js
@@ -0,0 +1,79 @@
+const defaultCharacter = {
+    name: '',
+    life: 1,
+    maxLife: 1,
+    attack: 0,
+    defense: 0
+}
+
+const createKnight = (name) => {
+    return {
+        ...defaultCharacter,
+        name,
+        life: 100,
+        maxLife: 100,
+        attack: 10,
+        defense: 8
+    }
+}
+
+const createSorcerer = (name) => {
+    return {
+        ...defaultCharacter,
+        name,
+        life: 50,
+        maxLife: 50,
+        attack: 14,
+        defense: 3
+    }
+}
+
+const createLittleMonster = () => {
+    return {
+        ...defaultCharacter,
+        name: 'Little Monster',
+        life: 40,
+        maxLife: 40,
+        attack: 4,
+        defense: 4
+    }
+}
+
+const createBigMonster = () => {
+    return {
+        ...defaultCharacter,
+        name: 'Big Monster',
+        life: 120,
+        maxLife: 120,
+        attack: 16,
+        defense: 6
+    }
+}
+
+const stage = {
+    start(fighter1, fighter2, fighter1El, fighter2El){
+        this.fighter1 = fighter1
+        this.fighter2 = fighter2
+        this.fighter1El = fighter1El
+        this.fighter2El = fighter2El
+
+        this.fighter1El = document.querySelector('#attackButtonFighter1').addEventListener('click', () => this.doAttack(this.fighter1, this.fighter2))
+        this.fighter2El = document.querySelector('#attackButtonFighter2').addEventListener('click', () => this.doAttack(this.fighter2, this.fighter1))
+
+        this.update()
+    },
+    update(){
+        // Fighter1
+        this.fighter1El = document.querySelector('#nameChar').innerHTML = `${this.fighter1.name} -- ${this.fighter1.life.toFixed(1)} HP`
+        let f1Pct = (this.fighter1.life / this.fighter1.maxLife) * 100
+        this.fighter1El = document.querySelector('#barChar').style.width = `${f1Pct}%`
+        // Fighter2
+        this.fighter2El = document.querySelector('#nameMonster').innerHTML = `${this.fighter2.name} -- ${this.fighter2.life.toFixed(1)} HP`
+        let f2Pct = (this.fighter2.life / this.fighter2.maxLife) * 100
+        this.fighter2El = document.querySelector('#barMonster').style.width = `${f2Pct}%`
+    },
+    doAttack(attacking, attacked){
+        console.log(`${attacking.name} atacando ${attacked.name}`)
+        this.update()
+    }
+}
---
+++ Exercícios/Exercício0015/assets/javascript/script.js
@@ -0,0 +1,9 @@
+const char = createKnight('Helder')
+const monster = createLittleMonster()
+
+stage.start(
+    char,
+    monster,
+    document.querySelector('#char'),
+    document.querySelector('#monster')
+)
