1b96d60026eaa54c6d9139b4bf3c94bd81d38d9f
Helder Meyer <helder.meyer3010@gmail.com>
2023-03-01 19:08:34 -0300

    Exercícios de Revisão

--- Exercícios/Exercício0016/assets/javascript/script.js
+++ Exercícios/Exercício0016/assets/javascript/script.js
@@ -11,3 +11,15 @@ console.log(formatar(preco))
 let cidade = 'São Paulo'
 console.log(cidade)
 
+let x = 10
+let y = 5
+console.log(x > y) // True
+
+let w = "10"
+let z = 10
+console.log(w === z) // False
+
+let a = 10
+let b = 5
+console.log(a > b) // True
+
